dbb53c7cb2882149d20921eff0cb09d3ed85f9e8
gitGyro-Dev <git.develop@gyro-wedge.com>
2026-06-29 12:27:19 +0900

    Create github_update.py
    c

---
+++ tools/github_update.py
@@ -0,0 +1,21 @@
+import requests
+import os
+
+OWNER = "gitGyro-Dev"
+REPO = "gyroauth"
+TOKEN = os.environ["GITHUB_TOKEN"]
+
+url = f"https://api.github.com/repos/{OWNER}/{REPO}/contents/README.md"
+
+headers = {
+    "Authorization": f"Bearer {TOKEN}",
+    "Accept": "application/vnd.github+json"
+}
+
+r = requests.get(url, headers=headers)
+
+print(r.status_code)
+
+data = r.json()
+
+print(data["sha"])
